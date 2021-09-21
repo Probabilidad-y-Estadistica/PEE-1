1d13167f5b13bfca8136c78652614e398bc10264
diegobergara <diegobj1998@gmail.com>
2021-09-20 22:25:27 -0300

    Progreso en ejericio 1

--- Project.R
+++ Project.R
@@ -13,11 +13,11 @@ boxplot(valoresN, miDistro, xlab = "Muestras",
         ylab = "Distribuciones", main = "Box plot de la primer distro")
 #b) Calcular los valores empiricos de la esperanza y de la varianza de cada una de las 4 muestras, y
 #compararlos con los valores teoricos correspondientes.
-var(miDistro) #Varianza es el cuadrado de la desviación estandar
-mean(miDistro) #Desviación estandar
-var(miDistroEmpirica) #Varianza empirica es el cuadrado de la desviación estandar
-sd(miDistroEmpirica) #Desviación estandar empirica
-#c) Presentar en un mismo gr´afico la funci´on de distribuci´on te´orica de la Distribucion 1 y la funcion
+var(miDistro) #Varianza es el cuadrado de la desviaciï¿½n estandar
+mean(miDistro) #Desviaciï¿½n estandar
+var(miDistroEmpirica) #Varianza empirica es el cuadrado de la desviaciï¿½n estandar
+sd(miDistroEmpirica) #Desviaciï¿½n estandar empirica
+#c) Presentar en un mismo grï¿½afico la funciï¿½on de distribuciï¿½on teï¿½orica de la Distribucion 1 y la funcion
 #de distribucion empirica de la muestra para n = 102 Hacer otro grafico similar para n = 105
 b = stepfun(c(1:10),miDistro)
 plot(b,verticals=FALSE)
@@ -25,12 +25,12 @@ plot(miDistroEmpirica1,add=T)
 plot(miDistroEmpirica2,add=T)
 
 
-#Máximo 8
+#Mï¿½ximo 8
 pbinom(8,10,0.15)
 #Lo mismo pero con un vector
 x<-c(0:8)
 sum(dbinom(x,10,0.15))
-#Más de 3 P=(x>3)=1-P(x<=3)
+#Mï¿½s de 3 P=(x>3)=1-P(x<=3)
 1-pbinom(3,10,0.15)
 #Graficar
 
@@ -38,3 +38,67 @@ x<-c(0:10)
 y<-dbinom(x,10,0.15)
 boxplot(x,y)
 
+# EJ 1
+# distribuciÃ³n binoamial binomial (30 ,  0.15)
+# n = 10 ^ (2 al 5)
+# rbinom(
+#       n,     NÃºmero de observaciones aleatorias a ser generadas
+#       size,  NÃºmero de ensayos (> = 0)
+#       prob)  La probabilidad de Ã©xito en cada ensayo
+x <- 1:30
+a = rbinom(x, 10^2, 0.15)
+b = rbinom(x, 10^3, 0.15)
+c = rbinom(x, 10^4, 0.15)
+d = rbinom(x, 10^5, 0.15)
+
+randomValues <- c(a,b,c,d)
+
+data <- data.frame(values =randomValues,
+                   group = c("10^2","10^3","10^4","10^5"))
+
+boxplot(values ~ group,
+        data, 
+        col = c("pink",
+                "beige", 
+                "lightblue", 
+                "lightgreen"),
+        main = "DistribuciÃ³n binomial",
+        xlab = "Ensayos",
+        ylab = "No se que es esto")
+
+
+plot(randomValues, type = "s", lwd = 2,
+     col = "lightgreen",
+     main = "DistribuciÃ³n binomial",
+     xlab = "Ensayos",
+     ylab = "No se que es esto")
+
+# Esperanza EmpÃ­rica
+espA = mean(a)
+espB = mean(b)
+espC = mean(c)
+espD = mean(d)
+
+# Esperanza Teorica
+espAT = 10^2 * 0.15
+espBT = 10^3 * 0.15
+espCT = 10^4 * 0.15
+espDT = 10^5 * 0.15
+
+# Varianza Teorixa
+varA = var(a)
+varB = var(b)
+varC = var(c)
+varD = var(d)
+
+# Varianza Teorica
+varAT = 10^2 * (1 - 0.15)
+varBT = 10^3 * (1 - 0.15)
+varCT = 10^4 * (1 - 0.15)
+varDT = 10^5 * (1 - 0.15)
+
+# DistribuciÃ³n acumulada empirica
+distA = ecdf(a)
+distD = ecdf(d)
+plot(distA)
+plot(distD)
