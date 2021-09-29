724e61d5223f65336e54f162494b6809d2725fdf
diegobergara <diegobj1998@gmail.com>
2021-09-28 21:49:17 -0300

    leyenda

--- Project.R
+++ Project.R
@@ -93,18 +93,43 @@ distD = ecdf(d)
 
 # Función de distribución teórica y empirica
 plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)), 
-     main = "Función de distribución", 
+     main = "Función de distribución acumulada", 
      xlab = "k",
      ylab = "F(k)",
      col="red")
 lines(distA, col="springgreen4")
+legend("bottomright", 
+       legend = c("Distribución Acumulada Empírica",
+                  "Distribución Acumulada Teórica"),
+       lty = 1, col = c("red","springgreen4"), lwd = 2, box.lty =1)
 
 plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)), 
-     main = "Función de distribución", 
+     main = "Función de distribución acumulada", 
      xlab = "k",
      ylab = "F(k)",
      col="red")
 lines(distD, col="blue")
+legend("bottomright", 
+       legend = c("Distribución Acumulada Empírica",
+                  "Distribución Acumulada Teórica"),
+       lty = 1, col = c("blue","red"), lwd = 2, box.lty =1)
 
+
+# Grid of X-axis values
+x <- 1:80
+
+# size = 80, prob = 0.2
+plot(dbinom(x, size = 80, prob = 0.2), type = "h", lwd = 2,
+     main = "Binomial probability function",
+     ylab = "P(X = x)", xlab = "Number of successes")
+
+# size = 80, prob = 0.3
+lines(dbinom(x, size = 80, prob = 0.3), type = "h",
+      lwd = 2, col = rgb(1,0,0, 0.7))
+
+# Add a legend
+legend("topright", legend = c("80  0.2", "80  0.3"),
+       title = "size  prob", title.adj = 0.95,
+       lty = 1, col = 1:3, lwd = 2, box.lty = 0)
 
 
