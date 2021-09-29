bcbe9984451ad24f60c5062dd981bc87b37e066c
diegobergara <diegobj1998@gmail.com>
2021-09-28 21:53:15 -0300

    clean up

--- Project.R
+++ Project.R
@@ -1,44 +1,3 @@
-#Distro binomial
-#p=0.15 q=0.85 n = 10^2, 10^3, 10^4 y 10^5
-help(pbinom)
-valores = c(0,1:10)
-valoresN <-c(10^2, 10^3, 10^4, 10^5)
-#Lo mismo pero con un vector
-miDistro <-pbinom(valores,valoresN,0.25) #Esto suma todas, si de deshace el vector, se puede calcular las puntuales
-miDistroEmpirica1 = ecdf(rbinom(50,10^2,0.25))
-miDistroEmpirica2 = ecdf(rbinom(50,10^5,0.25)) #Binomial con muestra de tam 50
-#ecdf muestra de distribucion acumulada empirica
-#a) Presentar en un mismo grafico los 4 boxplot (diagrama de cajas) de las muestras generadas.
-boxplot(valoresN, miDistro, xlab = "Muestras",
-        ylab = "Distribuciones", main = "Box plot de la primer distro")
-#b) Calcular los valores empiricos de la esperanza y de la varianza de cada una de las 4 muestras, y
-#compararlos con los valores teoricos correspondientes.
-var(miDistro) #Varianza es el cuadrado de la desviaci?n estandar
-mean(miDistro) #Desviaci?n estandar
-var(miDistroEmpirica) #Varianza empirica es el cuadrado de la desviaci?n estandar
-sd(miDistroEmpirica) #Desviaci?n estandar empirica
-#c) Presentar en un mismo gr?afico la funci?on de distribuci?on te?orica de la Distribucion 1 y la funcion
-#de distribucion empirica de la muestra para n = 102 Hacer otro grafico similar para n = 105
-b = stepfun(c(1:10),miDistro)
-plot(b,verticals=FALSE)
-plot(miDistroEmpirica1,add=T)
-plot(miDistroEmpirica2,add=T)
-
-
-#M?ximo 8
-pbinom(8,10,0.15)
-#Lo mismo pero con un vector
-x<-c(0:8)
-sum(dbinom(x,10,0.15))
-#M?s de 3 P=(x>3)=1-P(x<=3)
-1-pbinom(3,10,0.15)
-#Graficar
-
-x<-c(0:10)
-y<-dbinom(x,10,0.15)
-boxplot(x,y)
-
-
 # EJ 1
 # distribución binoamial binomial (30 ,  0.15)
 # n = 10 ^ (2 al 5)
@@ -99,7 +58,7 @@ plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)),
      col="red")
 lines(distA, col="springgreen4")
 legend("bottomright", 
-       legend = c("Distribución Acumulada Empírica",
+       legend = c("Distribución Acumulada Empírica - 10^2",
                   "Distribución Acumulada Teórica"),
        lty = 1, col = c("red","springgreen4"), lwd = 2, box.lty =1)
 
@@ -110,7 +69,7 @@ plot(stepfun(c(1:10),pbinom(c(0,1:10),30,0.15)),
      col="red")
 lines(distD, col="blue")
 legend("bottomright", 
-       legend = c("Distribución Acumulada Empírica",
+       legend = c("Distribución Acumulada Empírica - 10^5",
                   "Distribución Acumulada Teórica"),
        lty = 1, col = c("blue","red"), lwd = 2, box.lty =1)
 
